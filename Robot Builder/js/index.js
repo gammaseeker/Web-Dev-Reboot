f99f7cdb79d08c7d3ba31505e352820ccecdc6cc
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2018-01-03 19:33:07 -0500

    MVP finished

--- Robot Builder/js/index.js
+++ Robot Builder/js/index.js
@@ -3,14 +3,16 @@ function initialize(){
     shield = document.getElementById("shield");
     attack = document.getElementById("attack");
     mode = document.getElementById("mode");
+    ship = document.getElementById("ship");
     engine = document.getElementById("engine");
     ctr = document.getElementsByClassName("ctr");
     ctrArr = [0, 0, 0];
-
+    modeString = mode.innerHTML;
+    engString = engine.innerHTML;
 }
 
 function add(value, index){
-    if(ctrArr[index] >= 0){
+    if(ctrArr[index] >= 0 && (ctrArr[index] + value) > 0){
         ctrArr[index] += value;
         ctr[index].innerHTML = ctrArr[index];
     }
@@ -18,10 +20,12 @@ function add(value, index){
 
 function toggleMode(m){
     modeString = m;
+    display();
 }
 
 function selectEngine(eng){
     engString = eng;
+    display();
 }
 
 function display(){
