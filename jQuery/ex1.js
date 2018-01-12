3735ae14dccfd74bff635649fb8ad50ab44c206c
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2018-01-12 15:22:16 -0500

    Add files via upload

---
+++ jQuery/ex1.js
@@ -0,0 +1,6 @@
+$(document).ready( function(){
+      $("div").css("background", "purple");
+      $(".highlight").css("width", "200px");
+      $("#third").css("border", "2px solid orange");
+      $("div:nth-of-type(1)").css("color", "pink");
+});
