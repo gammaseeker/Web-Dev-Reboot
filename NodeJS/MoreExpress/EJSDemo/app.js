9342e71b90b973c94e55831822127a62facb679b
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2018-01-20 20:30:11 -0500

    Add files via upload

--- NodeJS/MoreExpress/EJSDemo/app.js
+++ NodeJS/MoreExpress/EJSDemo/app.js
@@ -1,16 +1,17 @@
 var express = require("express");
 var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
     //res.send("<h1>Welcome to the home page</h1>");
 });
 
-//Will look in views folder
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
     //res.send("You fell in love with " + thing);
-    res.render("love.ejs", {thingVar : thing});
+    res.render("love", {thingVar : thing});
 });
 
 app.get("/posts", function(req, res) {
@@ -19,7 +20,7 @@ app.get("/posts", function(req, res) {
        {title: "post 2", author: "Suz"},
        {title: "post 3", author: "Suy"}
        ]; 
-       res.render("posts.ejs", {posts:posts});
+       res.render("posts", {posts:posts});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
