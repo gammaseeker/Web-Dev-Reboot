d75fbae04054fac6689707e341583ed95a832c64
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2018-01-25 20:02:06 -0500

    Add files via upload

--- bank/app.js
+++ bank/app.js
@@ -1,1 +1,10 @@
-f
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+        res.render("home");
+});
+
+app.listen(8000, () => console.log('Listening on port 8000!'))
